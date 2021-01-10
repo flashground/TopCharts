8dbd19ce8807337d4d2235298a6c027bdeb96b08
Vlad <vladislavbiryukov@gmail.com>
2021-01-10 19:58:02 +0300

    add models, create db sqlite, add admin

---
+++ charts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Chart, Station
+
+@admin.register(Chart)
+class ChartAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    pass
---
+++ charts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+class Chart(models.Model):
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    data = models.TextField(null=False, blank=False)
+    added_date = models.DateField()
+    created_date = models.DateTimeField(auto_now_add=True)
